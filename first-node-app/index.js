578e3f438df575b60ae2f4aa1fcbbcd449b6c2bd
vinodh1988 <k.c.vinodh@hotmail.com>
2021-12-20 11:38:35 +0530

    home-commit

--- first-node-app/index.js
+++ first-node-app/index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const path = require('path');
 
 const app = express();
 
@@ -6,6 +7,9 @@ app.get("/",function(request,response){
     response.send("Hey Hi!!! Node is up and running")
 })
 
+app.get("/home",function(request,response){
+    response.sendFile(path.join(__dirname,"public/index.html"))
+})
 app.listen("4000",function(){
     console.log("Server is started on port 4000")
 })
