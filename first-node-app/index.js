d9697d9c3b1b8188ee8b5a20f266afc92f6f5f36
vinodh1988 <k.c.vinodh@hotmail.com>
2021-12-20 12:22:40 +0530

    parser-commit

--- first-node-app/index.js
+++ first-node-app/index.js
@@ -3,14 +3,27 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.urlencoded({
+    extended:true
+}))
+
+app.use(express.json())
+
 //allow direct access for the static resources present in the given pathv
 app.use(express.static(path.join(__dirname, 'public/styles')));
 
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+
 app.get("/",function(request,response){
     response.send("Hey Hi!!! Node is up and running")
+})
+
+app.post("/store",function(request,response){
+     console.log(request.body)
+     const {sno,name,city}=request.body
+     response.redirect("/home");
 })
 
 app.get("/home",function(request,response){
---
+++ fun.js
@@ -0,0 +1,9 @@
+
+
+const person ={sno:1,name:"raj",city:"Chennai"}
+
+console.log(person.sno,person.name,person.city);
+
+const {sno,name,city} = person;
+
+console.log(sno,name,city);
