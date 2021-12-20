38db72d91a8715771dc31f92552d5bd74543c4f5
vinodh1988 <k.c.vinodh@hotmail.com>
2021-12-20 11:58:34 +0530

    bootstrap

--- first-node-app/index.js
+++ first-node-app/index.js
@@ -3,6 +3,12 @@ const path = require('path');
 
 const app = express();
 
+//allow direct access for the static resources present in the given pathv
+app.use(express.static(path.join(__dirname, 'public/styles')));
+
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.get("/",function(request,response){
     response.send("Hey Hi!!! Node is up and running")
 })
